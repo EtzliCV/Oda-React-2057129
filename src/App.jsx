acc6f918f9a8a1766163d86c8faed183c2fc2986
Esly <eslysarhaycv1703@gmail.com>
2020-12-03 10:12:07 -0500

    curso

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./components/home";
 import Home2 from "./components/home2";
+import Curso from "./components/curso";
 
 function App() {
   return (
     <>
     <Home />
     <Home2 />
+    <Curso />
     </>
   );
 }
---
+++ src/components/curso/index.jsx
@@ -0,0 +1,95 @@
+import "./curso.css";
+import salsa from "../../assets/img/salsa.png";
+import bachata from "../../assets/img/bachata.png";
+import merengue from "../../assets/img/merengue.png";
+import afrodance from "../../assets/img/afrodance.png";
+import hiphop from "../../assets/img/hiphop.png";
+import reggaeton from "../../assets/img/reggaeton.png";
+import dancehall from "../../assets/img/dancehall.png";
+import commercial from "../../assets/img/commercial.png";
+
+function Curso() {
+    return (
+        <section className="curso pt-4 pb-5">
+            <div className="container text-center">
+                <h4 className="tituloA pb-2">Aprende</h4>
+                <p className="textoA mb-3">
+                    ¡Clases de baile desde casa! En ODA te mostramos los mejores vídeos
+          paso <br />
+          a paso para que tengas la oportunidad de formarte con los mejores.
+        </p>
+                <ul className="iconos1 row">
+                    <li className="mx-4">
+                        <div>
+                            <a href="./Generos/Salsa/index.html">
+                                <img src={salsa} width="150"/>
+                                </a>
+                        </div>
+                    </li>
+                    <li className="mx-4">
+                        <div>
+                            <a href="./Generos/Bachata/index.html"
+                            ><img src={bachata} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                    <li className="mx-4">
+                        <div>
+                            <a href="./Generos/Merengue/index.html"
+                            ><img src={merengue} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                    <li className="mx-4">
+                        <div>
+                            <a href="./Generos/Afrodance/index.html"
+                            ><img src={afrodance} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="iconos1 row">
+                    <li className="mx-2">
+                        <div>
+                            <a href="./Generos/Hiphop/index.html"
+                            ><img src={hiphop} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                    <li className="mx-2">
+                        <div>
+                            <a href="./Generos/Reggaeton/index.html"
+                            ><img src={reggaeton} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                    <li className="mx-2">
+                        <div>
+                            <a href="./Generos/Dancehall/index.html"
+                            ><img src={dancehall} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                    <li className="mx-4">
+                        <div>
+                            <a href="./Generos/Commercial/index.html"
+                            ><img src={commercial} width="150"
+                                /></a>
+                        </div>
+                    </li>
+                </ul>
+                <button
+                    type="button"
+                    class="btn btn-primary btn-lg px-4 my-2 boton2"
+                    href="https://app-vlc.hotmart.com/signup"
+                >
+                    Conoce más
+        </button>
+            </div>
+        </section>
+
+    );
+
+}
+
+export default Curso;
